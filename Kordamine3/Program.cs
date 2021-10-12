cff94babd7c88d775d5f85a7418e6fb4510f13f7
Opilane TTHK <opilane@tthk.ee>
2021-10-12 09:19:01 +0300

    Add project files.

---
+++ Kordamine3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Kordamine3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int mult = 1;
+            int sum = 0;
+            for (int i = 0; 1 < 5, i++)
+            {
+                Console.Write("Please enter your number; ");
+                int set = Convert.ToInt32(Console.ReadLine());
+                sum += set;
+                mult = set + mult;
+            }
+            double avg = sum / 5;
+            Console.WriteLine($"Nums AVG = (avg)\")*/
+
+            string w;
+
+            do
+            {
+
+                Console.WriteLine("Osta elevant!!!");
+                w = Console.ReadLine();
+
+            } while (w != "elevant");
+
+            if (w == "elevant")
+            {
+                Console.WriteLine("Väga hästi! Head aega!");
+            }
+
+        }
+    }
+}
---
+++ Kordamine3/ex2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kordamine3
+{
+    class ex2
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(1, 5);
+
+            for (int i; 1 < 5; i++)
+            {
+
+            }
+        }
+    }
+}
